28cb87ddff0c71667cd78d84d8f94be802582ceb
Karim Sami <karimsami047@gmail.com>
2022-05-18 01:13:36 +0200

    Add the logic to start a new game

--- app.js
+++ app.js
@@ -39,6 +39,12 @@ const app = Vue.createApp({
       else this.playerHealth = 100;
       this.attackPlayer();
     },
+    startNewGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.currentRound = 0;
+    },
   },
   computed: {
     monsterHealthBarStyles() {
