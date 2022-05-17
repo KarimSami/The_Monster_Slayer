a12947755a848b298da49771e05b96f5de1c7a76
Karim Sami <karimsami047@gmail.com>
2022-05-17 22:54:33 +0200

    Add data and methods to handle player and monster health and attacks

---
+++ app.js
@@ -0,0 +1,25 @@
+const getRandomValue = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+  methods: {
+    attackMonster() {
+      const attackDamage = getRandomValue(5, 12);
+      this.monsterHealth -= attackDamage;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackDamage = getRandomValue(8, 15);
+      this.playerHealth -= attackDamage;
+    },
+  },
+});
+
+app.mount("#game");
